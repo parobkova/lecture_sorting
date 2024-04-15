40edec280f79b4b05a8347ef13eb225dd22bc24f
parobkova <parobkovaterezia@gmail.com>
2024-04-15 08:59:00 +0200

    01

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -12,8 +13,43 @@ def read_data(file_name):
     file_path = os.path.join(cwd_path, file_name)
 
 
+    data = {}
+    with open(file_path,mode= 'r',  newline= '') as csvfile:
+        csv_read = csv.reader(csvfile)
+        headers = next(csv_read)
+
+
+        #pre kazdy stlpec prazdny zoznam v datovom slovniku
+        for header in headers:
+            data[header] = []
+
+        for row in csv_read:
+            for i, value in enumerate(row):
+                data[headers[i]].append(float(value))
+
+    return data
+
+
+def selection_sort(zoznam):
+    for i in range(len(zoznam)):
+        min_idx = i
+        for j in range(i+1, len(zoznam)):
+            if zoznam[j] < zoznam[min_idx]:
+                min_idx = j
+        (zoznam[i], zoznam[min_idx]) = (zoznam[min_idx], zoznam[i])
+
+    return zoznam
+
+
+
 def main():
-    pass
+    #pass
+    file_name = 'numbers.csv'
+    data = read_data(file_name)
+    data1 = data['series_1']
+    sel_sort = selection_sort(data1)
+    print(sel_sort)
+    #print(data)
 
 
 if __name__ == '__main__':
